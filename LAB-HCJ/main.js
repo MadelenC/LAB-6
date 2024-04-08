111dda6cba901d58cf8c0e80f10f5c9ee78a7e89
MadelenC <145080241+MadelenC@users.noreply.github.com>
2024-04-07 20:36:44 -0400

    Add files via upload
    laboratorio 6

---
+++ LAB-HCJ/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+const app = document.getElementById("root");
+
+const paragraph = document.createElement("p");
+paragraph.textContent = "TEXTO JS";
+
+app.appendChild(paragraph);
